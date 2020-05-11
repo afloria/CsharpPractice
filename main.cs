f160b7f99766224b1d86b72342c937ec6a096c8e
afloria <dreafloria@gmail.com>
2020-05-11 22:14:15 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MainClass {
+  public static void Main (string[] args) {
+    //Console.WriteLine ("Hello World");
+    Console.WriteLine("     /|");
+    Console.WriteLine("    / |");
+    Console.WriteLine("   /  |");
+    Console.WriteLine("  /___|");
+    
+    Console.ReadLine(); //prevents program from terminating immediately
+  }
+}
